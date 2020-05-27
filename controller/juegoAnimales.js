a123e85738e786f8851a24e010bd82ccea2b6f32
Marcos <madc_801@hotmail.com>
2020-05-27 07:20:25 -0300

    version 1 finalizada

--- controller/juegoAnimales.js
+++ controller/juegoAnimales.js
@@ -4,24 +4,27 @@ var b3=document.getElementById('button3');
 var b4=document.getElementById('button4');
 
 
-
-
-
 function Toque(b)
 {	
 	let url_image;
+	let sonido;
 	switch (b.dataset.animal) {
 		case "gatito":
 			url_image='url(../images/gato.jpg)';
+			sonido=document.getElementById("sound_gato");
+
 			break;
 		case "perrito":
 			url_image='url(../images/perro.jpg)';
+			sonido=document.getElementById("sound_perro");
 			break;
 		case "caballito":
 			url_image='url(../images/caballo.jpg)';
+			sonido=document.getElementById("sound_caballo");
 			break;
 		case "vaquita":
 			url_image='url(../images/vaca.jpg)';
+			sonido=document.getElementById("sound_vaca");
 			break;
 		default:
 			// statements_def
@@ -31,8 +34,10 @@ function Toque(b)
 	
 	console.log("comence a ejecutar");
 	b.style.backgroundImage =url_image;
+	sonido.play();
 	setTimeout(function(){
 		b.style.backgroundImage = 'none';
+	 	sonido.pause();
 		},1500);
 	console.log("deje de ejecutar");
 	
